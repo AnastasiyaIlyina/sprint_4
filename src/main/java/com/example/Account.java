0899bbe949a9c9348c884e64b85e17102081636c
AnastasiyaIlyina <nastasia.ami@yandex.ru>
2022-01-12 00:10:36 +0300

    Добавлены два класса Покрыт тестами класс Account

---
+++ src/main/java/com/example/Account.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class Account {
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public boolean checkNameToEmboss() {
+        return name.length() >= 3 &&
+                name.length() <= 19 &&
+                !name.startsWith(" ") &&
+                !name.endsWith(" ") &&
+                name.chars().filter(ch -> ch == ' ').count() == 1;
+    }
+}
